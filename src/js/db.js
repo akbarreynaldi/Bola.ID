b11e546f7e96281b7a306b59801b6c83691a8ba0
akbarreynaldi <akbarreynaldi77@gmail.com>
2020-06-29 20:49:48 +0700

    database match initialisasi

---
+++ src/js/db.js
@@ -0,0 +1,62 @@
+var dbPromised = idb.open("real-madrid", 1, function(upgradeDb) {
+    var matchObjectStore = upgradeDb.createObjectStore("match", {
+        keyPath: "id"
+    });
+    matchObjectStore.createIndex("match_matchday", "match_matchday", { unique: false });
+});
+
+function saveForLater(data) {
+    dbPromised
+        .then(function(db) {
+            var tx = db.transaction("match", "readwrite");
+            var store = tx.objectStore("match");
+            console.log(data);
+            store.add(data.matches[0]);
+            console.log(data.matches[0].id);
+            return tx.complete;
+        })
+        .then(function() {
+            console.log("Match detail berhasil di simpan.");
+        });
+}
+
+function getAll() {
+    return new Promise(function(resolve, reject) {
+        dbPromised
+            .then(function(db) {
+                var tx = db.transaction("match", "readonly");
+                var store = tx.objectStore("match");
+                return store.getAll();
+            })
+            .then(function(match) {
+                resolve(match);
+            });
+    });
+}
+
+function getById(id) {
+    return new Promise(function(resolve, reject) {
+        dbPromised
+            .then(function(db) {
+                var tx = db.transaction("match", "readonly");
+                var store = tx.objectStore("match");
+                return store.get(id);
+            })
+            .then(function(match) {
+                resolve(match);
+            });
+    });
+}
+
+function deleteSaved(data) {
+    dbPromised
+        .then(function(db) {
+            var tx = db.transaction("match", "readwrite");
+            var store = tx.objectStore("match");
+            store.delete(data.matches[0].id);
+            return tx.complete;
+        })
+        .then(function() {
+            console.log("Match detail berhasil di hapus.");
+        });
+}
