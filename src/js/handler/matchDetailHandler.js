c4518a6e2256e370a10d02d5bee350f4b75426d0
akbarreynaldi <akbarreynaldi77@gmail.com>
2020-07-04 07:15:40 +0700

    memperbaiki mode offline untuk halaman match detail

--- src/js/handler/matchDetailHandler.js
+++ src/js/handler/matchDetailHandler.js
@@ -1,46 +1,53 @@
 import { getMatchById, getSavedMatchById } from '../handler/matchDataHandler.js';
 
-
+let urlParams = new URLSearchParams(window.location.search);
+let isFromSaved = urlParams.get("saved");
+//mengambil id elemen untuk mengatur penampilan button
+let btnBackMatch = document.getElementById("backToMatch");
+let btnBackSaved = document.getElementById("backToSaved");
+let btnSave = document.getElementById("save");
+let btnRemove = document.getElementById("remove");
 
 document.addEventListener("DOMContentLoaded", function() {
-    //mengambil query dari url
-    let urlParams = new URLSearchParams(window.location.search);
-    let isFromSaved = urlParams.get("saved");
 
-    //mengambil id elemen untuk mengatur penampilan button
-    let btnBackMatch = document.getElementById("backToMatch");
-    let btnBackSaved = document.getElementById("backToSaved");
-    let btnSave = document.getElementById("save");
-    let btnRemove = document.getElementById("remove");
+    //pengecekan kondisi berdasarkan query url halaman
+    if (isFromSaved) {
+        // Hide fab save jika dimuat dari halaman saved
+        btnSave.style.display = 'none';
+        btnBackMatch.style.display = 'none';
+    } else {
+        // Hide fab remove jika dimuat dari halaman match
+        btnRemove.style.display = 'none';
+        btnBackSaved.style.display = 'none';
+    }
 
-    const ifConnected = window.navigator.onLine;
-    if (ifConnected) {
+});
+
+
+let xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState === 4) {
+        const content = document.querySelector("#body-content");
+        const match = document.querySelector("match-details");
+
         //pengecekan kondisi berdasarkan query url halaman
         if (isFromSaved) {
-            // Hide fab save jika dimuat dari halaman saved
-            btnSave.style.display = 'none';
-            btnBackMatch.style.display = 'none';
-
             // ambil match data by id dari indexedDB lalu tampilkan
             getSavedMatchById();
         } else {
-            // Hide fab remove jika dimuat dari halaman match
-            btnRemove.style.display = 'none';
-            btnBackSaved.style.display = 'none';
-
             // ambil match data by id dari cache jika ada atau melakukan fetch request lalu tampilkan
             getMatchById();
         }
-    } else {
-        btnSave.style.display = 'none';
-        btnRemove.style.display = 'none';
-        btnBackSaved.style.display = 'none';
-        let notConnectedHTML = `
-        <p id="not-connected">You are in offline mode, cannot get data from server API!</p>
-        `;
-        let notConnectedElem = document.getElementById("match-details");
-        if (notConnectedElem !== null) {
-            document.getElementById("match-details").innerHTML = notConnectedHTML;
+        console.log(xhttp.responseText);
+        console.log(this.status);
+        if (this.status === 200) {
+            match.innerHTML = xhttp.responseText;
+        } else if (this.status === 404) {
+            content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
+        } else {
+            content.innerHTML = `<p id="not-connected">You are in offline mode, cannot get data from server API!</p>`;
         }
     }
-});
+};
+xhttp.open("GET", "match-detail.html", true);
+xhttp.send();
